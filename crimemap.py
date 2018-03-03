f2a287f02b8bd4f9cbcefd4c36e68fef7c388ef3
Zhangweijun <ahnqihyun@sina.cn>
2018-03-03 15:34:05 +0800

    add validation on crimemap

--- crimemap.py
+++ crimemap.py
@@ -1,4 +1,8 @@
 import json
+import string
+import datetime
+
+import dateparser
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -8,40 +12,67 @@ from dbhelper import DBHelper
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
+def format_date(userdate):
+	date = dateparser.parse(userdate)
+	try:
+		return datetime.datetime.strftime(date,"%Y-%d-%m")
+	except TypeError:
+		return None
+
+def sanitize_string(userinput):
+	whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+	return ''.join(filter(lambda x: x in whitelist, userinput))
+
 @app.route('/')
-def home():
+def home(error_message=None):
 	try:
 		crimes = DB.get_all_crimes()
 		crimes_json = json.dumps(crimes)
 	except Exception as e:
 		print(e)
 		data = None
-	return render_template('home.html', data=crimes_json)
+	return render_template('home.html',
+						 	data=crimes_json,
+						 	categories=categories,
+						 	error_message=error_message)
 
-@app.route('/add', methods=['POST'])
-def add():
-	try:
-		data = request.form.get('userinput')
-		DB.add_input(data)
-	except Exception as e:
-		print(e)
-	return home()
+# @app.route('/add', methods=['POST'])
+# def add():
+# 	try:
+# 		data = request.form.get('userinput')
+# 		DB.add_input(data)
+# 	except Exception as e:
+# 		print(e)
+# 	return home()
 
-@app.route('/clear')
-def clear():
-	try:
-		DB.clear_all()
-	except Exception as e:
-		print(e)
-	return home()
+# @app.route('/clear')
+# def clear():
+# 	try:
+# 		DB.clear_all()
+# 	except Exception as e:
+# 		print(e)
+# 	return home()
 
 @app.route('/submitcrime', methods=['POST'])
 def submitcrime():
 	category = request.form.get("category")
-	date = request.form.get("date")
-	latitude = float(request.form.get("latitude"))
-	longitude = float(request.form.get("longitude"))
-	description = request.form.get("description")
+	if category not in categories:
+		return home()
+
+	date = format_date(request.form.get("date"))
+	if not date:
+		return home("Invalid date. Please use yyyy-mm-dd format")
+
+	try:
+		latitude = float(request.form.get("latitude"))
+		longitude = float(request.form.get("longitude"))
+	except ValueError:
+		return home()
+	# description = request.form.get("description")
+	description = sanitize_string(request.form.get("description"))
+	print(description)
 	DB.add_crime(category, date, latitude, longitude, description)
 	return home()
 
